ff56f269b959f7861d0bd2cf08cdc5144dcebcc4
Hyunghun <webofthink@snu.ac.kr>
2016-03-30 14:14:00 +0900

    initial commit

---
+++ RedirectHome/server.js
@@ -0,0 +1,8 @@
+﻿var http = require('http');
+var port = process.env.port || 80;
+http.createServer(function (req, res) {
+    res.writeHead(301,
+        { Location: 'https://sites.google.com/site/connectedxlab/home' }
+    );
+    res.end();
+}).listen(port);
